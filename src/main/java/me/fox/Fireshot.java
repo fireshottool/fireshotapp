a4d4c011aa06f4c3a09a325b463a9f91ef256b91
Fox Foxed <foxfoxed297@gmail.com>
2020-10-21 15:39:28 +0200

    initial commit

---
+++ src/main/java/me/fox/Fireshot.java
@@ -0,0 +1,13 @@
+package me.fox;
+
+/**
+ * @author (Ausgefuchster)
+ * @version (2.0 ~ 21.10.2020)
+ */
+
+public class Fireshot {
+
+    public static void main(String[] args) {
+        System.out.println("Fireshot");
+    }
+}
