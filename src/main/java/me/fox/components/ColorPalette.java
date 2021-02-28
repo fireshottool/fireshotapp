e98339ea29710aae7d345d9f0e0672d7b1fba67e
Daspoet <cederk2306@gmail.com>
2021-02-28 19:17:50 +0100

    Make Strokes and ColorPalette final

--- src/main/java/me/fox/components/ColorPalette.java
+++ src/main/java/me/fox/components/ColorPalette.java
@@ -7,7 +7,7 @@ import java.awt.*;
  * @version (~ 09.01.2021)
  */
 
-public class ColorPalette {
+public final class ColorPalette {
     public static final Color DARK_BLUE_200 = new Color(5, 5, 25, 200);
     public static final Color DARK_BLUE_170 = new Color(5, 5, 25, 170);
     public static final Color DARK_BLUE_LIGHTER_200 = new Color(45, 45, 65, 200);
--- src/main/java/me/fox/components/Strokes.java
+++ src/main/java/me/fox/components/Strokes.java
@@ -7,7 +7,7 @@ import java.awt.*;
  * @version (~ 09.01.2021)
  */
 
-public class Strokes {
+public final class Strokes {
     public static final Stroke WIDTH_ONE_STROKE = new BasicStroke(1);
     public static final Stroke WIDTH_TWO_STROKE = new BasicStroke(2);
 }
